d64e6a55ef524bbdcd99761b4c26e57e8c6a9328
chebishev <atanas.chebishev@gmail.com>
2024-02-07 17:44:33 +0200

    add resources

---
+++ 08. Exercise - Infrastructure as Code/01. Azure Recource Group/azure-rg.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.90.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "myResourceGroup"
+  location = "eastus"
+}
